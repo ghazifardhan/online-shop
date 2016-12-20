028ea63ee60dd9a26a797de3415e66b19edf1b4f
myzpolice <ghazifardhan@gmail.com>
2016-12-20 22:36:48 +0700

    first commit

---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/online-shop/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/online-shop/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/online-shop/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/online-shop/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/online-shop/upload/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/online-shop/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/online-shop/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/online-shop/upload/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/online-shop/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/online-shop/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/online-shop/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/online-shop/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/online-shop/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
